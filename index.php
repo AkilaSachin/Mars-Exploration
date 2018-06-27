ed6a76b7aa81d7c36deb1f27a93de076af061c36
DESKTOP-345D7DV\Akila Jayasinghe <akilajayasinghe@gmail.com>
2018-06-27 21:37:33 +0530

    connect mdbboostrap

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
     <meta charset=utf-8>
     <title>Mars Exploration</title>
 	
-    <link href=http://fonts.googleapis.com/css?family=Droid+Sans:400,700%7cDroid+Serif:400,700,400italic,700italic%7cYellowtail%7cGreat+Vibes rel=stylesheet type=text/css>
-    
-    <!-- <link rel=stylesheet id=main-style-file type=text/css href=assets/css/styles.css> -->
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
 
+    <!-- Sweet Alert -->
     <script src="dist/sweetalert-dev.js"></script>
     <link rel="stylesheet" href="dist/sweetalert.css">
 
@@ -23,6 +29,21 @@
 
     <h1>My First Heading</h1>
     <p>My first paragraph.</p>
+
+
+
+
+
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
 
 </body>
 
